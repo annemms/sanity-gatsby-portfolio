dcb76813d8488c75045934490151efd21c051d4f
Anne Marie Samuelsen <annemariesa2308@gmail.com>
2022-01-11 22:43:18 +0100

    images array

--- studio/schemas/documents/sampleProject.js
+++ studio/schemas/documents/sampleProject.js
@@ -1,77 +1,84 @@
-import {format} from 'date-fns'
+/* eslint-disable quotes */
+import { format } from "date-fns";
 
 export default {
-  name: 'sampleProject',
-  title: 'Malerier',
-  type: 'document',
+  name: "sampleProject",
+  title: "Malerier",
+  type: "document",
   fields: [
     {
-      name: 'title',
-      title: 'Title',
-      type: 'string'
+      name: "title",
+      title: "Title",
+      type: "string"
     },
     {
-      name: 'slug',
-      title: 'Slug',
-      type: 'slug',
-      description: 'Some frontend will require a slug to be set to be able to show the project',
+      name: "slug",
+      title: "Slug",
+      type: "slug",
+      description: "Dette er urlen til maleriet",
       options: {
-        source: 'title',
+        source: "title",
         maxLength: 96
       }
     },
     {
-      name: 'publishedAt',
-      title: 'Produksjonsdato',
-      type: 'datetime'
+      name: "publishedAt",
+      title: "Produksjonsdato",
+      type: "datetime"
     },
     {
-      name: 'price',
-      title: 'Pris',
-      type: 'string'
+      name: "price",
+      title: "Pris",
+      type: "string"
     },
     {
-      name: 'format',
-      title: 'Format',
-      type: 'string'
+      name: "format",
+      title: "Format",
+      type: "string"
     },
     {
-      name: 'mainImage',
-      title: 'Main image',
-      type: 'figure'
+      name: "mainImage",
+      title: "Main image",
+      type: "figure"
     },
     {
-      name: 'body',
-      title: 'Beskrivelse',
-      type: 'projectPortableText'
+      name: "imagesGallery",
+      title: "Bildekarusell",
+      type: "array",
+      of: [{ type: "image" }]
     },
     {
-      name: 'isSold',
-      title: 'Solgt',
-      type: 'boolean'
+      name: "body",
+      title: "Beskrivelse",
+      type: "projectPortableText"
     },
     {
-      name: 'relatedProjects',
-      title: 'Related malerier',
-      type: 'array',
-      of: [{type: 'reference', to: {type: 'sampleProject'}}]
+      name: "isSold",
+      title: "Solgt",
+      type: "boolean"
+    },
+    {
+      name: "relatedProjects",
+      title: "Related malerier",
+      type: "array",
+      of: [{ type: "reference", to: { type: "sampleProject" } }]
     }
   ],
   preview: {
     select: {
-      title: 'title',
-      publishedAt: 'publishedAt',
-      slug: 'slug',
-      media: 'mainImage'
+      title: "title",
+      publishedAt: "publishedAt",
+      slug: "slug",
+      media: "mainImage"
     },
-    prepare ({title = 'No title', publishedAt, slug = {}, media}) {
-      const dateSegment = format(publishedAt, 'YYYY/MM')
-      const path = `/${dateSegment}/${slug.current}/`
+    prepare({ title = "No title", publishedAt, slug = {}, media }) {
+      const dateSegment = format(publishedAt, "YYYY/MM");
+      const path = `/${dateSegment}/${slug.current}/`;
       return {
         title,
         media,
-        subtitle: publishedAt ? path : 'Missing publishing date'
-      }
+        subtitle: publishedAt ? path : "Missing publishing date"
+      };
     }
   }
-}
+};
