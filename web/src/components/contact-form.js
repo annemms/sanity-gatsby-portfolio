857f63a74b00a46d3f59781109c70a2a3f338063
Anne Marie Samuelsen <annemariesa2308@gmail.com>
2022-05-02 18:33:30 +0200

    added form validation

--- web/src/components/contact-form.js
+++ web/src/components/contact-form.js
@@ -1,5 +1,18 @@
-import React from "react";
+import React, { useRef } from "react";
 import styled from "styled-components";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+const schema = yup.object().shape({
+  firstname: yup.string().required(),
+  lastname: yup.string().required(),
+  email: yup
+    .string()
+    .email()
+    .required(),
+  message: yup.string().required()
+});
 
 const FormWrapper = styled.div`
   background: #7990a1;
@@ -25,9 +38,13 @@ const Form = styled.form`
     font-size: 20px;
     min-width: 100%;
     max-width: 100%;
+    font-family: "Raleway";
   }
   textarea {
     width: 100%;
+    font-family: "Raleway";
+    font-weight: 400;
+    font-size: 20px;
   }
   p {
     color: #fff;
@@ -55,50 +72,93 @@ const SectionWrapper = styled.div`
   }
 `;
 
-const ContactForm = () => (
-  <FormWrapper id="contact">
-    <Form
-      method="post"
-      action="https://www.flexyform.com/f/775f49998b81280972c5397758219bb463534882"
-    >
-      <h2>Kontakt meg</h2>
-      <SectionWrapper>
+const ContactForm = () => {
+  const {
+    register,
+    trigger,
+    handleSubmit,
+    formState: { errors },
+    reset
+  } = useForm({
+    resolver: yupResolver(schema)
+  });
+
+  const $form = useRef(null);
+
+  const onSubmitHandler = async (data, e) => {
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data)
+    };
+
+    const response = await fetch(
+      "https://www.flexyform.com/f/775f49998b81280972c5397758219bb463534882",
+      requestOptions
+    );
+    const jsonData = await response.json();
+
+    console.log({ jsonData, data });
+
+    /*
+
+
+    console.log({ data, form });
+    setTimeout(() => {
+      reset();
+    }, 1000);
+    */
+  };
+  return (
+    <FormWrapper id="contact" ref={$form}>
+      <Form
+        onSubmit={handleSubmit(onSubmitHandler)}
+        method="post"
+        action="https://www.flexyform.com/f/775f49998b81280972c5397758219bb463534882"
+      >
+        <h2>Kontakt meg</h2>
+        <SectionWrapper>
+          <div>
+            <label>
+              <p>Fornavn</p>
+              <input {...register("firstname")} type="firstname" name="firstname" id="firstname" />
+            </label>
+            <p>{errors.firstname?.message}</p>
+          </div>
+          <div>
+            <label>
+              <p>Etternavn</p>
+              <input {...register("lastname")} type="lastname" name="lastname" id="lastname" />
+            </label>
+            <p>{errors.lastname?.message}</p>
+          </div>
+        </SectionWrapper>
+        <SectionWrapper>
+          <div>
+            <label>
+              <p>E-post</p>
+              <input {...register("email")} type="email" name="email" id="email" />
+            </label>
+            <p>{errors.email?.message}</p>
+          </div>
+          <div>
+            <label>
+              <p>Telefon</p>
+              <input {...register("phone")} type="phone" name="phone" id="phone" />
+            </label>
+          </div>
+        </SectionWrapper>
         <div>
           <label>
-            <p>Fornavn</p>
-            <input type="text" name="name" id="name" />
+            <p>Melding</p>
+            <textarea {...register("message")} name="message" id="message" rows="5" />
           </label>
+          <p>{errors.message?.message}</p>
         </div>
-        <div>
-          <label>
-            <p>Etternavn</p>
-            <input type="text" name="name" id="name" />
-          </label>
-        </div>
-      </SectionWrapper>
-      <SectionWrapper>
-        <div>
-          <label>
-            <p>E-post</p>
-            <input type="email" name="email" id="email" />
-          </label>
-        </div>
-        <div>
-          <label>
-            <p>Telefon</p>
-            <input type="text" name="subject" id="subject" />
-          </label>
-        </div>
-      </SectionWrapper>
-      <div>
-        <label>
-          <p>Melding</p>
-          <textarea name="message" id="message" rows="5" />
-        </label>
-      </div>
-      <button type="submit">Send</button>
-    </Form>
-  </FormWrapper>
-);
+        <button type="submit">Send</button>
+      </Form>
+    </FormWrapper>
+  );
+};
 
 export default ContactForm;
